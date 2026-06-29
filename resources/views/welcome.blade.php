fdda4c9830678063391ea423ea92837aaf5e1786
Pasupathi Infovenz <pasupathi@infovenz.com>
2026-06-29 20:11:59 +0530

    updated code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,6 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-      <h1>Welcome to laravel - VPS Server Hosting
+      <h1>Welcome to laravel - VPS Server Hosting - Changes pushed</h1>
     </body>
 </html>
