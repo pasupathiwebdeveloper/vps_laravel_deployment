db1535cbf9300d4ebb36fc8903a6c69e3f0fc275
Pasupathi Infovenz <pasupathi@infovenz.com>
2026-07-04 14:51:39 +0530

    version 3 updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,6 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-      <h1>Welcome to laravel - VPS Server Hosting - Changes pushed - version 2</h1>
+      <h1>Welcome to laravel - VPS Server Hosting - Changes pushed - version 3</h1>
     </body>
 </html>
